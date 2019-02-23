7fb8eb9dafe931c6154636f6fd94e167e16de594
Sara Walker <sarawall1187@gmail.com>
2019-02-23 17:44:59 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -15,7 +15,7 @@ class Application
       search_term = req.params["q"]
       resp.write handle_search(search_term)
       elsif req.path.match(/cart/)
-        
+        cart_search
     else
       resp.write "Path Not Found"
     end
