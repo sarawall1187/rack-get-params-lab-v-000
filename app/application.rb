7919ae6f6d95baa2afe1934295aa14ab899950a4
Sara Walker <sarawall1187@gmail.com>
2019-02-23 17:56:48 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -17,7 +17,7 @@ class Application
       resp.write handle_search(search_term)
     elsif req.path.match(/cart/)
      binding.pry
-      cart_search
+      resp.write cart_search
      
     else
       resp.write "Path Not Found"
